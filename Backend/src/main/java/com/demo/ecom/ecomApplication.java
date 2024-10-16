e4cec2a5545fd844f54a9f80c47d7041d6159e3f
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-17 00:01:47 +0530

    first commit

---
+++ Backend/src/main/java/com/demo/ecom/ecomApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ecomApplication.class, args);
+	}
+
+}
