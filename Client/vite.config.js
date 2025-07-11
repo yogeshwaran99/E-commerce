a34e4e795dec556a507211ecbf0bc8829a1eafbe
yogeshwaran99 <itsyogeshwaran@gmail.com>
2025-07-12 00:04:51 +0530

    feat: dockerize client, server, database, and nginx setup

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,15 +2,6 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  plugins: [react()],    
 })
 
